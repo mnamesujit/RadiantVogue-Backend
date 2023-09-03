22c059e234b7b4a8f86e390e377ba3500968f0f1
Sujit <mnamesujit@gmail.com>
2023-09-03 19:37:41 +0530

    controller for product filteration based on thier status

--- controllers/productController.js
+++ controllers/productController.js
@@ -11,7 +11,47 @@ const getProducts = async (req, res) => {
   }
 };
 
+const getPendingProducts = async (req, res) => {
+  try {
+    const results = await Connection.promise().query(
+      'SELECT * FROM products WHERE status = "pending"'
+    );
+    res.status(200).json({ pendingProducts: results[0] });
+  } catch (err) {
+    console.error("Error fetching pending products:", err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+const getApprovedProducts = async (req, res) => {
+  try {
+    const results = await Connection.promise().query(
+      'SELECT * FROM products WHERE status = "approved"'
+    );
+    res.status(200).json({ pendingProducts: results[0] });
+  } catch (err) {
+    console.error("Error fetching pending products:", err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+const approveProduct = async (req, res) => {
+  const productId = req.params.productId;
+
+  try {
+    await Connection.promise().query(
+      'UPDATE products SET status = "approved" WHERE product_id = ?',
+      [productId]
+    );
+    res.status(200).json({ message: "Product approved successfully" });
+  } catch (err) {
+    console.error("Error approving product:", err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 module.exports = {
   getProducts,
+  getPendingProducts,
+  getApprovedProducts,
+  approveProduct
 }
--- routes/product.js
+++ routes/product.js
@@ -1,10 +1,13 @@
 // routes/product.js
 const express = require('express');
 const router = express.Router();
-const {getProducts} = require('../controllers/productController');
+const {getProducts, getPendingProducts, getApprovedProducts, approveProduct} = require('../controllers/productController');
 const checkAuthToken = require('../middlewares/auth');
 
 // routes for product-related operations
 router.get('/', getProducts);
+router.get('/pending-products', checkAuthToken, getPendingProducts);
+router.get('/approved-products', checkAuthToken, getApprovedProducts);
+router.post('/approve-product/:productId', checkAuthToken, approveProduct);
 
 module.exports = router;
