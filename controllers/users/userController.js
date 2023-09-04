05fe562476d5e351e1992fd3e932009a8ce7c30d
Sujit <mnamesujit@gmail.com>
2023-09-04 22:05:58 +0530

    fixed the duplicate registration logic

--- controllers/users/userController.js
+++ controllers/users/userController.js
@@ -2,37 +2,38 @@ require('dotenv').config()
 const bcrypt = require("bcrypt");
 const jwt = require('jsonwebtoken');
 const SECRET_KEY = process.env.JWT_TOKEN;
-const Connection = require('../../config/dbConnection')
+const db = require('../../config/dbConnection')
 
 const register = async (req, res) => {
     try {
       const { username, email, password } = req.body;
       
+      console.log(req.body)
       // Hashing the user's password
       const hashedPassword = await bcrypt.hash(password, 10);
   
       // Check if the username already exists in the database
       const query = 'SELECT * FROM users WHERE email = ?';
-      const values = [username]
-      Connection.query(query, values, async (err, results) => {
+      const values = [email]
+      db.query(query, values, (err, results) => {
         if (err) {
           console.error('Error executing SQL query:', err);
           return res.status(500).json({ message: 'Internal server error' });
         }
-  
+        console.log("hey user exists")
         if (results.length > 0) {
-          return res.status(400).json({ message: 'Username already exists' });
+          return res.status(400).json({ message: 'Accout already exists' });
         }
   
         // Insert the new user into the database
-        const insertQuery = 'INSERT INTO users (username,email, password, user_type) VALUES (?, ?, ?)';
+        const insertQuery = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
         const values = [username, email, hashedPassword]
-        Connection.query(insertQuery, values, (err) => {
+        db.query(insertQuery, values, (err) => {
           if (err) {
             console.error('Error executing SQL query:', err);
             return res.status(500).json({ message: 'Internal server error' });
           }
-  
+          console.log("user Created successfylly--------------------")
           res.status(201).json({ message: 'User registered successfully' });
         });
       });
@@ -51,7 +52,7 @@ const login = async (req, res) => {
     // Find the user in the database
     const query = 'SELECT * FROM users WHERE email = ?';
     const values = [email]
-    Connection.query(query, values, async (err, results) => {
+    db.query(query, values, async (err, results) => {
       if (err) {
         console.error('Error executing SQL query:', err);
         return res.status(500).json({ message: 'Internal server error' });
@@ -81,16 +82,13 @@ const login = async (req, res) => {
 }
 
 
-
-
-
 const profile = (req, res) => {
   res.json({ message: "Authenticated route", user: req.user });
 };
 
 
 module.exports = {
-    login,
-    register,
-    profile
+  login,
+  register,
+  profile
 }
